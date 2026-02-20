e0cd636057d09789a40c767af1e2babb5ea555d1
Ads4331x <karkitan@gmail.com>
2026-02-20 07:43:21 +0545

    added missed files

--- backend/tts_speak.py
+++ backend/tts_speak.py
@@ -1,15 +1,28 @@
-from TTS.api import TTS
-import subprocess
-import os
-import time
-import re
 import warnings
 import logging
+import sys
+import os
+
+# Completely silence TTS
+os.environ['TTS_LOG_LEVEL'] = 'ERROR'
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
 warnings.filterwarnings("ignore")
-logging.getLogger("TTS").setLevel(logging.ERROR)
+logging.getLogger().setLevel(logging.ERROR)
 
-tts_engine = TTS("tts_models/en/ljspeech/tacotron2-DDC")
+# Redirect stdout temporarily during TTS init
+old_stdout = sys.stdout
+sys.stdout = open(os.devnull, 'w')
+
+from TTS.api import TTS
+import subprocess
+import time
+import re
+
+tts_engine = TTS("tts_models/en/ljspeech/vits")
+
+sys.stdout.close()
+sys.stdout = old_stdout
 
 def tts_speak(text):
     sentences = re.split(r'(?<=[.!?]) +', text)
@@ -17,7 +30,15 @@ def tts_speak(text):
         sentence = sentence.strip()
         if sentence:
             audio_file = f'/tmp/voice_{int(time.time())}.wav'
+            
+            old_stdout = sys.stdout
+            sys.stdout = open(os.devnull, 'w')
+            
             tts_engine.tts_to_file(text=sentence, file_path=audio_file)
+            
+            sys.stdout.close()
+            sys.stdout = old_stdout
+            
             subprocess.run(['aplay', '-q', audio_file], check=False)
             try:
                 os.remove(audio_file) 
